3e728786c74a503eadfedbe413031a86509f9be1
spd yeung <ccspeedy4@gmail.com>
2020-05-23 23:20:45 -0700

    EDIT: applied stock_api

--- stock_analyze/settings.py
+++ stock_analyze/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stock_API',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'stock_analyze.urls'
--- stock_analyze_client/src/components/home.js
+++ stock_analyze_client/src/components/home.js
@@ -1,146 +1,77 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Chart } from 'react-charts';
+import { Line } from 'react-chartjs-2';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import './Home.css';
-import { simpleAction } from '../actions/simpleAction';
+import { DEVELOPMENT_URL } from '../constants/urls';
 
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      series: { showPoints: true },
-      axes: []
+      data: {},
+      options: {}
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    fetch(DEVELOPMENT_URL + "Stock_API/")
+      .then(response => response.json())
+      .then(data => {
+        const { date, price, name } = data[0];
+        this.setState({
+          data: {
+            labels: date.reverse(),
+            datasets: [
+              {
+                label: name,
+                borderColor: 'rgb(255, 99, 132)',
+                data: price.reverse(),
+              }
+            ]
+          }
+        });
+      });
+
     this.setState({
-      data: [
-        {
-          label: "Series 1",
-          datums: [
+      options: {
+        scales: {
+          xAxes: [
             {
-              x: new Date("2020-03-18T11:00:00.000Z"),
-              y: 60
-            },
-            {
-              x: new Date("2020-03-18T11:30:00.000Z"),
-              y: 23
-            },
-            {
-              x: new Date("2020-03-18T12:00:00.000Z"),
-              y: 65
-            },
-            {
-              x: new Date("2020-03-18T12:30:00.000Z"),
-              y: 84
-            },
-            {
-              x: new Date("2020-03-18T13:00:00.000Z"),
-              y: 87
-            },
-            {
-              x: new Date("2020-03-18T13:30:00.000Z"),
-              y: 84
-            },
-            {
-              x: new Date("2020-03-18T14:00:00.000Z"),
-              y: 96
-            },
-            {
-              x: new Date("2020-03-18T14:30:00.000Z"),
-              y: 88
-            },
-            {
-              x: new Date("2020-03-18T15:00:00.000Z"),
-              y: 63
-            },
-            {
-              x: new Date("2020-03-18T15:30:00.000Z"),
-              y: 60
+              ticks: {}
             }
-          ]
-        },
-        {
-          label: "Series 2",
-          datums: [
+          ],
+          yAxes: [
             {
-              x: new Date("2020-03-18T11:00:00.000Z"),
-              y: 41
-            },
-            {
-              x: new Date("2020-03-18T11:30:00.000Z"),
-              y: 15
-            },
-            {
-              x: new Date("2020-03-18T12:00:00.000Z"),
-              y: 95
-            },
-            {
-              x: new Date("2020-03-18T12:30:00.000Z"),
-              y: 96
-            },
-            {
-              x: new Date("2020-03-18T13:00:00.000Z"),
-              y: 33
-            },
-            {
-              x: new Date("2020-03-18T13:30:00.000Z"),
-              y: 96
-            },
-            {
-              x: new Date("2020-03-18T14:00:00.000Z"),
-              y: 32
-            },
-            {
-              x: new Date("2020-03-18T14:30:00.000Z"),
-              y: 49
-            },
-            {
-              x: new Date("2020-03-18T15:00:00.000Z"),
-              y: 18
-            },
-            {
-              x: new Date("2020-03-18T15:30:00.000Z"),
-              y: 69
+              ticks: {},
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Price'
+              }
             }
           ]
         }
-      ]
-    });
-    this.setState({
-      axes: [
-        { primary: true, type: 'time', position: 'bottom' },
-        { type: 'linear', position: 'left' }
-      ]
+      }
     });
   }
 
 	render() {  
-    
-    const { data, axes, series } = this.state;
-
+    const { data, options } = this.state;
 		return (
       <Container>
         <Row>
           <Col md = {{ span: 10, offset: 1 }}>
-            <div className = "HomeContainer">
-              {data.length > 0 && axes.length > 0 && (
-                <Chart
-                  data={data}
-                  series={series}
-                  axes={axes}
-                  tooltip
-                  primaryCursor
-                  secondaryCursor
-                />
-              )}
+            <div>
+              <Line
+                data={data}
+                options={options}
+                height={500}
+                width={700}
+              />
             </div>
           </Col>
         </Row>
